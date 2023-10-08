fc8949b176432311957e6a0e9d52ed92befa7ddd
Favo4gabo <137852920+Favo4gabo@users.noreply.github.com>
2023-10-08 13:18:51 +0000

    primer fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue,setInputValue] = useState( { label: "", done: false},)
+	const [todos,setTodos] = useState([])
+
+	const deleteTask = (id) => {
+		let newArray = todos.filter((item, index) => index !== id);
+		setTodos(newArray);
+	}
+	const addTask = () => {
+		fetch('https://playground.4geeks.com/apis/fake/todos/user/gabriel', {
+      method: "PUT",
+      body: JSON.stringify(todos),
+      headers: {
+        "Content-Type": "application/json"
+      }
+	}
+		)}
+
+	function updateList (updateList) {
+		fetch('https://playground.4geeks.com/apis/fake/todos/user/gabriel', {
+      method: "PUT",
+      body: JSON.stringify(todos),
+      headers: {
+        "Content-Type": "application/json"
+      }
+	}
+
+
 	return (
 		<div className="text-center">
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
